052f39321393c33b22c40400abf2e2d12de3026a
survivorbat <djbatcat@gmail.com>
2018-01-10 09:45:05 +0100

    Changed person to student

--- router.js
+++ router.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 /* Subroutes */
-//const personapi = require('./routes/personapi.js');
-//router.use('/api/person/', personapi);
+const studentapi = require('./routes/studentapi.js');
+router.use('/api/student/', personapi);
+
+router.use((error,req,res,next) => {
+	res.status(500).send({
+		error: error
+	}).end();
+});
 
 /* Catch any undefined routes with a 404 status*/
 router.get('*', (req, res) => {
