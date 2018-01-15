2c5def04feddb5f3859d17a4c3179e961231115f
Gerben Droogers <gerben@droogers.net>
2018-01-15 09:50:47 +0100

    Fix formatting

--- controllers/ctrl_meal.js
+++ controllers/ctrl_meal.js
@@ -21,82 +21,66 @@ function bodyContainsAllFields(body, withID) {
 
 
 module.exports = {
-    getAll(req, res, next){
-        db.query('SELECT ' + fields + ' FROM Meals', function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-            res.status(200).send(results);
-        });
-    },
-    getById(req,res, next){
-        if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id'])) {
-            res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
-            return;
-        }
-        db.query('SELECT ' + fields + ' from Meals WHERE ID = ?', [req.params['id']], function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-            res.status(200).send(results);
-          });
-    },
-    create(req,res,next){
-        if(!bodyContainsAllFields(req.body, false)){
-            console.log('ERROR 400',req.body);
-            res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
-            return;
-        }
-        db.query('INSERT INTO Meals (' + fields + ') VALUES (?,?,?,?,?,?,?,?)', [null,req.body['Dish'],req.body['DateTime'],req.body['Info'],req.body['ChefID'],req.body['Price'],req.body['MaxFellowEaters'],req.body['DoesCookEat']], function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-            res.status(201).send(results);
-          });
-    },
-    update(req,res,next){
-        if(!bodyContainsAllFields(req.body, true)){
-            console.log('400',req.body);
-            res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
-            return;
-        }
-        db.query('UPDATE Meals SET Dish = ?, DateTime = ?, Info =?, ChefID = ?, Price = ?, MaxFellowEaters = ?, DoesCookEat = ? WHERE ID = ?', [req.body['Dish'],req.body['DateTime'],req.body['Info'],req.body['ChefID'],req.body['Price'],req.body['MaxFellowEaters'],req.body['DoesCookEat'],req.body['ID']], function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-            res.status(200).send(results);
-          });
-    },
-    delete(req,res,next){
-        if(req.body['ID'] === undefined || req.body['ID'] === "" || isNotNumeric(req.body['ID'])){
-            res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
-            return;
-        } else {
-        db.query('DELETE FROM Meals WHERE ID = ?', [req.body['ID']], function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-            res.status(200).send(results);
-          });
-        }
-    },
-		setByIdThePicture(req,res,next){
-			if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id']) || req.body['Picture'] === undefined) {
-				res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+	getAll(req, res, next){
+		db.query('SELECT ' + fields + ' FROM Meals', function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
 				return;
-			}
-			var img = new Buffer(req.body['Picture'], 'base64');
-			db.query('UPDATE Meals SET Picture = ? WHERE ID = ?', [img,req.params['id']], function (error, results, fields) {
+			};
+			res.status(200).send(results);
+		});
+	},
+	getById(req,res, next){
+		if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id'])) {
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		}
+		db.query('SELECT ' + fields + ' from Meals WHERE ID = ?', [req.params['id']], function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
+				return;
+			};
+			res.status(200).send(results);
+		});
+	},
+	create(req,res,next){
+		if(!bodyContainsAllFields(req.body, false)){
+			console.log('ERROR 400',req.body);
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		}
+		db.query('INSERT INTO Meals (' + fields + ') VALUES (?,?,?,?,?,?,?,?)', [null,req.body['Dish'],req.body['DateTime'],req.body['Info'],req.body['ChefID'],req.body['Price'],req.body['MaxFellowEaters'],req.body['DoesCookEat']], function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
+				return;
+			};
+			res.status(201).send(results);
+		});
+	},
+	update(req,res,next){
+		if(!bodyContainsAllFields(req.body, true)){
+			console.log('400',req.body);
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		}
+		db.query('UPDATE Meals SET Dish = ?, DateTime = ?, Info =?, ChefID = ?, Price = ?, MaxFellowEaters = ?, DoesCookEat = ? WHERE ID = ?', [req.body['Dish'],req.body['DateTime'],req.body['Info'],req.body['ChefID'],req.body['Price'],req.body['MaxFellowEaters'],req.body['DoesCookEat'],req.body['ID']], function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
+				return;
+			};
+			res.status(200).send(results);
+		});
+	},
+	delete(req,res,next){
+		if(req.body['ID'] === undefined || req.body['ID'] === "" || isNotNumeric(req.body['ID'])){
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		} else {
+			db.query('DELETE FROM Meals WHERE ID = ?', [req.body['ID']], function (error, results, fields) {
 				if (error){
 					console.log(error);
 					res.status(500).send(error);
@@ -104,20 +88,36 @@ module.exports = {
 				};
 				res.status(200).send(results);
 			});
-		},
-		getByIdThePicture(req,res, next){
-        if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id'])) {
-            res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
-            return;
-        }
-        db.query('SELECT picture from Meals WHERE ID = ?', [req.params['id']], function (error, results, fields) {
-            if (error){
-                console.log(error);
-                res.status(500).send(error);
-                return;
-            };
-						res.setHeader('Content-Type', 'image/png' );
-            res.status(200).send(results[0]['picture']);
-          });
-    },
+		}
+	},
+	setByIdThePicture(req,res,next){
+		if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id']) || req.body['Picture'] === undefined) {
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		}
+		var img = new Buffer(req.body['Picture'], 'base64');
+		db.query('UPDATE Meals SET Picture = ? WHERE ID = ?', [img,req.params['id']], function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
+				return;
+			};
+			res.status(200).send(results);
+		});
+	},
+	getByIdThePicture(req,res, next){
+		if(req.params['id'] === undefined || req.params['id'] === "" || isNotNumeric(req.params['id'])) {
+			res.status(400).send({message:'Missing or wrong parameters! Please refer to the documentation'}).end();
+			return;
+		}
+		db.query('SELECT picture from Meals WHERE ID = ?', [req.params['id']], function (error, results, fields) {
+			if (error){
+				console.log(error);
+				res.status(500).send(error);
+				return;
+			};
+			res.setHeader('Content-Type', 'image/png' );
+			res.status(200).send(results[0]['picture']);
+		});
+	},
 }
